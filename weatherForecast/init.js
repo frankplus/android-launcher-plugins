34f6c562c38e1a2ea8b5f200df09cff0e3aae8c2
Arvind <arvind.yadav@tutanota.com>
2023-09-16 19:26:42 +0530

    Changed VollaPluginList.json format and added weather and wikipedia plugin structure

---
+++ weatherForecast/init.js
@@ -0,0 +1,37 @@
+function metadata() {
+    'name': 'Weather Forecast',
+    'description': 'It will add feature to get Weather Forecast directly from Springboard',
+    'version': 0.1,
+    'minLauncherVersion':,
+    'maxLauncherVersion':
+
+}
+
+
+/*
+This is sample code need to execute at client side to start this script
+const item = init("testString");
+const methodNames = Object.keys(item);
+const methods = Object.values(item);
+console.log("returned item "+ methodNames + "  "+methods);
+if (typeof item[methodNames] === 'function') {
+    console.log(`Executing ${methodNames}: ${item[methodNames]()}`);
+}
+*/
+
+
+function init(inputParameter){
+    const stringProcessor = processInput(inputParameter);
+    return stringProcessor;
+}
+
+
+function processInput(inputString) {
+    // Process the input string here
+    // Todo : Need to validate for city anme and need to check if weather forecast app is installed
+    // Return an object containing the methods/functions
+    return {
+        "Weather": function(inputString) { // todo: Need to launch weather forecast app. }
+    };
+}
+
